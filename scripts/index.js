fe46cf461cdce2172f8c656d1dec50fcb5b016d9
Steph Mendoza <sstephanie_mc@hotmail.com>
2026-06-03 17:18:04 -0500

    part 2 of the sprint 6 project

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,47 @@ const initialCards = [
 initialCards.forEach(function (card) {
   console.log(card.name);
 });
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+const popUp = document.querySelector("#edit-popup");
+const closePopUp = popUp.querySelector(".popup__close");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const popUpInputTypeName = document.querySelector(".popup__input_type_name");
+const popUpInputTypeDescription = document.querySelector(
+  ".popup__input_type_description",
+);
+const popUpForm = document.querySelector(".popup__form");
+const formElement = document.querySelector("#edit-profile-form");
+
+function openModal(firstModal) {
+  firstModal.classList.add("popup_is-opened");
+}
+function closeModal(secondModal) {
+  secondModal.classList.remove("popup_is-opened");
+}
+
+closePopUp.addEventListener("click", function () {
+  closeModal(popUp);
+});
+
+function fillProfileForm() {
+  popUpInputTypeName.value = profileTitle.textContent;
+  popUpInputTypeDescription.value = profileDescription.textContent;
+}
+
+function handleOpenEditModal() {
+  fillProfileForm();
+  openModal(popUp);
+}
+
+profileEditButton.addEventListener("click", handleOpenEditModal);
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = popUpInputTypeName.value;
+  profileDescription.textContent = popUpInputTypeDescription.value;
+  closeModal(popUp);
+}
+
+formElement.addEventListener("submit", handleProfileFormSubmit);
