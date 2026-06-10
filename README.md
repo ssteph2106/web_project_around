e318b875b27f44e4cda1101f4bde811588516b69
Steph Mendoza <sstephanie_mc@hotmail.com>
2026-06-09 23:49:53 -0500

    final project, sprint 6

--- README.md
+++ README.md
@@ -5,3 +5,7 @@ En este proyecto se ha trabajado con un conjunto de funciones, elementos y objet
 He reforzado conocimientos sobre cómo declarar funciones, trabajar con manipulación de elementos de un formulario, cómo hacer que los botones tengan vida y sean dinámicos.
 
 Todavía falta la tercera parte del proyecto, pero todo va marchando bien.
+
+En esta última parte del proyecto se trabajó con un template, del cual usando varias funciones se logró crear el código correcto para interactuar con los elementos de la página web, se usó una función forEach para que se automatizara la creación de las tarjetas a partir de un array de objetos.
+
+También se habilitó el uso de botones "like", "delete" y crear nuevas tarjetas añadiendolas al inicio
--- scripts/index.js
+++ scripts/index.js
@@ -40,6 +40,10 @@ const popUpInputTypeDescription = document.querySelector(
 );
 const popUpForm = document.querySelector(".popup__form");
 const formElement = document.querySelector("#edit-profile-form");
+const imagePopUpId = document.querySelector("#image-popup");
+const popUpImage = document.querySelector(".popup__image");
+const popUpCaption = document.querySelector(".popup__caption");
+const popUpCloseBtn = imagePopUpId.querySelector(".popup__close");
 
 function openModal(firstModal) {
   firstModal.classList.add("popup_is-opened");
@@ -50,6 +54,10 @@ function closeModal(secondModal) {
 
 closePopUp.addEventListener("click", function () {
   closeModal(popUp);
+});
+
+popUpCloseBtn.addEventListener("click", () => {
+  closeModal(imagePopUpId);
 });
 
 function fillProfileForm() {
@@ -72,3 +80,74 @@ function handleProfileFormSubmit(evt) {
 }
 
 formElement.addEventListener("submit", handleProfileFormSubmit);
+
+const cardTemplate = document
+  .querySelector("#template")
+  .content.querySelector(".card");
+
+const cardsList = document.querySelector(".cards__list");
+
+function getCardElement(
+  name = "Sin título",
+  link = "./images/placeholder.jpg",
+) {
+  const cardElement = cardTemplate.cloneNode(true);
+  const cardName = cardElement.querySelector(".card__title");
+  const cardImage = cardElement.querySelector(".card__image");
+  const cardLikeBtn = cardElement.querySelector(".card__like-button");
+  const cardDeletebtn = cardElement.querySelector(".card__delete-button");
+
+  cardLikeBtn.addEventListener("click", (evt) => {
+    evt.target.classList.toggle("card__like-button_is-active");
+  });
+
+  cardDeletebtn.addEventListener("click", () => {
+    cardElement.remove();
+  });
+
+  cardImage.addEventListener("click", (evt) => {
+    popUpImage.alt = name;
+    popUpCaption.textContent = name;
+    popUpImage.src = link;
+    openModal(imagePopUpId);
+  });
+
+  cardName.textContent = name;
+  cardImage.src = link;
+  cardImage.alt = name;
+
+  return cardElement;
+}
+
+function renderCard(cardsList, name, link) {
+  cardsList.prepend(getCardElement(name, link));
+}
+
+initialCards.forEach(function (card) {
+  renderCard(cardsList, card.name, card.link);
+});
+
+const profileAddBtn = document.querySelector(".profile__add-button");
+const newCardPopUp = document.querySelector("#new-card-popup");
+const newPopUpForm = document.querySelector("#new-card-form");
+const savePopUpBtn = document.querySelector("#new-card-popup .popup__button");
+const closePopUpBtn = document.querySelector("#new-card-popup .popup__close");
+const popUpInputTypeCardName = document.querySelector(
+  ".popup__input_type_card-name",
+);
+const popUpInputTypeUrl = document.querySelector(".popup__input_type_url");
+
+profileAddBtn.addEventListener("click", function () {
+  openModal(newCardPopUp);
+});
+closePopUpBtn.addEventListener("click", function () {
+  closeModal(newCardPopUp);
+});
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+  renderCard(cardsList, popUpInputTypeCardName.value, popUpInputTypeUrl.value);
+  closeModal(newCardPopUp);
+}
+
+newPopUpForm.addEventListener("submit", handleCardFormSubmit);
